6eb4f607dd62096d96cf17bbcadab0ac27af0a52
Arian Jay <priscoarianjay159@gmail.com>
2024-10-13 09:16:27 +0800

    13th commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,25 @@
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
-    extend: {},
-    fontFamily: {
-      Inter: ["Inter"]
-    },
+    extend: {
+      fontFamily: {
+        Inter: ["Inter"]
+      },
+      colors: {
+        coffee: {
+            50: '#FDF8F6',
+            100: '#F2E8E5',
+            200: '#EADDD7',
+            300: '#E0CEC7',
+            400: '#D2BAA9',
+            500: '#C4A68A',
+            600: '#B58863',
+            700: '#A66E40',
+            800: '#8C5E3D',
+            900: '#704F32',
+        },
+      }
+  },
   },
   plugins: [
     require('@tailwindcss/aspect-ratio'),
